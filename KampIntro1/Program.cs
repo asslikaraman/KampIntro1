2a2bf7366da6f2a0849021889d257790af75bb90
Asli Nur Karaman <karamanasli7868@gmail.com>
2022-06-04 21:40:12 +0300

    Add project files.

---
+++ KampIntro1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KampIntro1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.kursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.izlenmeOrani = 64;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.kursAdi = "Java";
+            kurs2.Egitmen = "Mert";
+            kurs2.izlenmeOrani = 70;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.kursAdi = "Python";
+            kurs3.Egitmen = "Engin Demiroğ";
+            kurs3.izlenmeOrani = 85;
+            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3 };
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.kursAdi + " : " +kurs.Egitmen);
+
+            }
+            Console.ReadLine();
+
+
+
+        }
+    }
+    public class Kurs
+    {
+        public string kursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int izlenmeOrani { get; set; }
+    }
+        
+}
---
+++ MetotlarKamp/SepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotlarKamp
+{
+    internal class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Ürün sepete eklendi-"+urun.Adi);
+        }
+
+    }
+}
